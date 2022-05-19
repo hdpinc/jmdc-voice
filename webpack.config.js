54b0b890bed3feb36506ece3a7615a27e22b0581
k-kawane <k.kawane.1231@gmail.com>
2022-05-19 19:30:39 +0900

    開発環境作成

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const PATH = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "[name].js",
+  },
+  devServer: {
+    static: {
+      directory: PATH.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: "./archive.html",
+      template: "./src/archive.html",
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: "./entry.html",
+      template: "./src/entry.html",
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/style.css",
+    }),
+    new RemoveEmptyScriptsPlugin(),
+  ],
+};
